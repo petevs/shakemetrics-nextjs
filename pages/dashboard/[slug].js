ba6bcf96871187d800c2e5f7596d1d8cd938ba1e
Pete VS <pete_vs88@me.com>
2022-02-18 13:38:36 -0700

    added static paths for dashboard routes

--- pages/dashboard/[slug].js
+++ pages/dashboard/[slug].js
@@ -1,6 +1,22 @@
 import { Title } from '@mantine/core'
 import { useRouter } from 'next/router'
 import DashboardShell from '../../components/DashboardShell'
+import { dashboardItems } from '../../data/navItems'
+
+
+export async function getStaticPaths(){
+    
+    const paths = dashboardItems.map(item => ({
+        params: { slug: (item.title).toLowerCase()}
+    }))
+    
+    
+    return {
+        paths,
+        fallback: false
+    }
+}
+
 
 const DashboardPage = () => {
 
@@ -9,7 +25,10 @@ const DashboardPage = () => {
     
     return(
         <DashboardShell>
-            <Title>{slug}</Title>
+            <Title
+            >
+                {slug}
+            </Title>
         </DashboardShell>
     )
 }
