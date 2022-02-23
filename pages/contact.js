53a5ddbabab83ff8869a2ba47d1cdb6b3179698c
Pete VS <pete_vs88@me.com>
2022-02-23 16:08:37 -0700

    added header tags

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import BasicPageLayout from "../components/BasicPageLayout"
-import { Divider, Text } from '@mantine/core'
+import { Text } from '@mantine/core'
+import Head from "next/head"
 
 const Contact = () => {
 
@@ -14,51 +15,57 @@ const Contact = () => {
     })
 
     return (
-        <BasicPageLayout
-            title='Contact'
-            firstSection={
-                <>
-                    <Text 
-                        size='lg' 
-                        weight={500} 
-                    >
-                        Email
-                    </Text>
-                    <Text 
-                        size='md' 
-                        color='dimmed'
-                        sx={linkStyle}
-                    >
-                        <a href='mailto:hello@shakemetrics.com?subject=Inquiry from the site'>
-                            hello@shakemetrics.com
-                        </a>
-                    </Text>
-                    <Text 
-                        size='lg' 
-                        weight={500} 
-                        mt='xl'
-                    >
-                        Mailing Address
-                    </Text>
-                    <Text 
-                        size='md'
-                        color='dimmed'
-                        sx={{lineHeight: '1.3rem'}}
-                    >
-                        Shake Metrics <br />
-                        329 Howe St PMB 2020<br />
-                        Vancouver, BC V6C 3N2
-                    </Text>
-                    <Text size='xs' color='dimmed' mt='xs'>*Please note this is a mailing address only. There are no employees or people affiliated with the site at this location.</Text>
-                    <Text 
-                        size='md' 
-                        color='dimmed'
-                    >
-                        
-                    </Text>
-                </>
-            }
-        />
+        <>
+            <Head>
+                <title>Contact</title>
+                <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+            </Head>
+            <BasicPageLayout
+                title='Contact'
+                firstSection={
+                    <>
+                        <Text 
+                            size='lg' 
+                            weight={500} 
+                        >
+                            Email
+                        </Text>
+                        <Text 
+                            size='md' 
+                            color='dimmed'
+                            sx={linkStyle}
+                        >
+                            <a href='mailto:hello@shakemetrics.com?subject=Inquiry from the site'>
+                                hello@shakemetrics.com
+                            </a>
+                        </Text>
+                        <Text 
+                            size='lg' 
+                            weight={500} 
+                            mt='xl'
+                        >
+                            Mailing Address
+                        </Text>
+                        <Text 
+                            size='md'
+                            color='dimmed'
+                            sx={{lineHeight: '1.3rem'}}
+                        >
+                            Shake Metrics <br />
+                            329 Howe St PMB 2020<br />
+                            Vancouver, BC V6C 3N2
+                        </Text>
+                        <Text size='xs' color='dimmed' mt='xs'>*Please note this is a mailing address only. There are no employees or people affiliated with the site at this location.</Text>
+                        <Text 
+                            size='md' 
+                            color='dimmed'
+                        >
+                            
+                        </Text>
+                    </>
+                }
+            />
+        </>
     )
 }
 
--- pages/faq.js
+++ pages/faq.js
@@ -1,23 +1,30 @@
 import { Accordion } from "@mantine/core"
 import BasicPageLayout from "../components/BasicPageLayout"
 import { frequentlyAskedQuestions } from "../lib/frequentlyAskedQuestions"
+import Head from "next/head"
 
 const FAQPage = () => {
   return (
-    <BasicPageLayout
-      title='Frequently Asked Questions'
-      firstSection={
-        <Accordion>
-        {
-          frequentlyAskedQuestions.map( (item, idx) => (
-            <Accordion.Item key={idx} label={item.q}>
-              {item.a}
-            </Accordion.Item>
-          ))
+    <>
+      <Head>
+          <title>Frequently Asked Questions</title>
+          <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+      </Head>
+      <BasicPageLayout
+        title='Frequently Asked Questions'
+        firstSection={
+          <Accordion>
+          {
+            frequentlyAskedQuestions.map( (item, idx) => (
+              <Accordion.Item key={idx} label={item.q}>
+                {item.a}
+              </Accordion.Item>
+            ))
+          }
+          </Accordion>
         }
-        </Accordion>
-      }
-    />
+      />
+    </>
   )
 }
 
