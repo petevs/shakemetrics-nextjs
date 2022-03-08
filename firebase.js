1656e08d08e946f4eb4d1d17a443948871443681
Pete VS <pete_vs88@me.com>
2022-03-08 08:52:55 -0700

    installed google analytics

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 import { getFunctions } from 'firebase/functions'
-
+import { getAnalytics, logEvent } from 'firebase/analytics'
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,6 +13,7 @@ const firebaseConfig = {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 const createFirebaseApp = (config) => {
@@ -31,4 +32,11 @@ const db = getFirestore(firebaseApp);
 const storage = getStorage(firebaseApp);
 const functions = getFunctions(firebaseApp)
 
-export { db, auth, storage, functions };
+let analytics
+if(firebaseApp.name && typeof window !== 'undefined'){
+  analytics = getAnalytics()
+  logEvent(analytics, 'notification_received')
+}
+
+
+export { db, auth, storage, functions, analytics };
