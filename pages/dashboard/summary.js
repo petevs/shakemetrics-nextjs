906f39e2b18ace277f70b7c21eaf2131735bee5d
Pete VS <pete_vs88@me.com>
2022-03-19 12:52:43 -0600

    added summary page

---
+++ pages/dashboard/summary.js
@@ -0,0 +1,15 @@
+import { Box } from '@mantine/core'
+import DashboardShell from '../../components/DashboardShell'
+import PageHeader from '../../components/PageHeader'
+
+const SummaryPage = () => {
+    return(
+        <>
+            <DashboardShell>
+                <PageHeader title='Summary' />
+            </DashboardShell>
+        </>
+    )
+}
+
+export default SummaryPage
