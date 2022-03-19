e6e963e4b2271ed70c54674ada4a56e39c286077
Pete VS <pete_vs88@me.com>
2022-03-19 13:15:41 -0600

    added more to summary page

--- pages/dashboard/summary.js
+++ pages/dashboard/summary.js
@@ -1,5 +1,6 @@
-import { Box } from '@mantine/core'
+import { Box, Paper, Text } from '@mantine/core'
 import DashboardShell from '../../components/DashboardShell'
+import DbSelectionSection from '../../components/DbSelectionSection'
 import PageHeader from '../../components/PageHeader'
 
 const SummaryPage = () => {
@@ -7,6 +8,31 @@ const SummaryPage = () => {
         <>
             <DashboardShell>
                 <PageHeader title='Summary' />
+                <Paper
+                    shadow='sm'
+                    radius='md'
+                    withBorder
+                    padding='xl'
+                >
+                    <DbSelectionSection>
+                        <Text size='lg' mb='sm'>Activity</Text>
+                    </DbSelectionSection>
+                    <Text size='sm' transform='uppercase'>
+                        Purchases
+                    </Text>
+                    <Text size='sm' ml='sm'>
+                        Bitcoin
+                    </Text>
+                    <Text size='sm' ml='lg'>
+                        Quantity
+                    </Text>
+                    <Text size='sm' ml='lg'>
+                        Amount Invested
+                    </Text>
+                    <Text size='sm' ml='sm'>
+                        Ethereum
+                    </Text>
+                </Paper>
             </DashboardShell>
         </>
     )
